c07c00bdb06e1db72ac94aea9ba990b01eb7253a
Artem Shchirov <artemshchirov@gmail.com>
2024-03-16 16:01:54 +0200

    feat(web): add login page

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="p-10">
+    <h1 class="mb-10 text-2xl font-bold">Syncer CRM</h1>
+  </div>
+</template>
---
+++ pages/login.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+useHead({
+  title: "Login | Syncer CRM",
+  meta: [
+    {
+      name: "description",
+      content: "Login to Syncer CRM",
+    },
+  ],
+});
+
+const emailRef = ref("");
+const passwordRef = ref("");
+const nameRef = ref("");
+</script>
+
+<template>
+  <div class="flex items-center justify-center w-full min-h-screen">
+    <div class="w-1/4 p-5 rounded bg-sidebar">
+      <h1 class="mb-5 text-2xl font-bold text-center">Login</h1>
+
+      <form>
+        <UiInput placeholder="Email" type="email" class="mb-3" v-model="emailRef" />
+        <UiInput placeholder="Password" type="email" class="mb-3" v-model="passwordRef" />
+        <UiInput placeholder="Name" type="email" class="mb-3" v-model="nameRef" />
+        <div class="flex items-center justify-center gap-5">
+          <UiButton type="button">Login</UiButton>
+          <UiButton type="button">Register</UiButton>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
