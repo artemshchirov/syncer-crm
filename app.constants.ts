e83573ee0f61dc3e1a2fb189e2cfb9531d764c54
Artem Shchirov <artemshchirov@gmail.com>
2024-03-16 12:53:55 +0200

    feat(server): add appwrite

---
+++ app.constants.ts
@@ -0,0 +1,7 @@
+// WARN: move all to env
+export const APP_WRITE_ID = "syncer-crm";
+export const DB_ID = "syncer-crm-db";
+export const COLLECTION_DEALS = "deals";
+export const COLLECTION_CUSTOMERS = "customers";
+export const COLLECTION_COMMENTS = "comments";
+export const STORAGE_ID = "";
